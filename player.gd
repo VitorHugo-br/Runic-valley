8a050d27c79b369e794078276f32015fbf30c664
VitorHugo-br <vitor.comercialcontato@gmail.com>
2026-01-16 01:32:29 -0300

    0002 chore: modifying folders structure

--- player.gd
+++ player.gd
@@ -1,11 +1,9 @@
 extends CharacterBody2D
 
-# Configurações de movimento normal
 @export var speed = 500.0
 
-# Configurações do Dash
 @export var dash_speed = 2000.0
-@export var dash_duration = 0.15
+@export var dash_duration = 0.18
 @export var dash_cooldown = 0.8
 
 var is_dashing = false
@@ -14,12 +12,17 @@ var can_dash = true
 func _physics_process(_delta):
 	if is_dashing:
 		move_and_slide()
-		return
 
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = direction * speed
 	move_and_slide()
 	
+	if Input.is_action_just_pressed("run"):
+		start_run()
+		
+	if Input.is_action_just_released("run"):
+		speed = 500
+		
 	if Input.is_action_just_pressed("dash") and can_dash and direction != Vector2.ZERO:
 		start_dash(direction)
 
@@ -34,3 +37,6 @@ func start_dash(direction):
 	
 	await get_tree().create_timer(dash_cooldown).timeout
 	can_dash = true
+	
+func start_run():
+	speed = 800
